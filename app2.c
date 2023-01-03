5da5532f1d9312983a60b97642ced5801807f687
Rodin <rodinhart@gmail.com>
2023-01-03 18:55:47 +0000

    WIP

--- app2.c
+++ app2.c
@@ -2,8 +2,8 @@ typedef unsigned int uint;
 typedef char *string;
 
 typedef struct {
-    uint length;
-    void *elements;
+    uint len;
+    void *items;
 } Vector;
 
 typedef struct {
@@ -13,19 +13,46 @@ typedef struct {
 
 typedef struct _Element {
     string tag;
-    Vector props;
+    Vector *props;
     string text;
 } Element;
 
-void toXml(string buffer, Element el) {
-    uint p = 0;
-    buffer[p++] = '<';
+uint string_copy(string des, string src) {
+    int p = 0;
+    while (src[p] != 0) {
+        des[p] = src[p];
+        p++;
+    }
+
+    return p;
 }
 
-void render(string buffer) {
-    Pair pairs[1] = { "style", "color: red;" };
-    Vector props = { 1, pairs };
-    Element header = { "h2", props, "Hello World" };
+uint toXml(string buffer, Element *el) {
+    uint p = 0;
+    buffer[p++] = '<';
+    p += string_copy(buffer + p, el->tag);
 
-    toXml(buffer, header);
+    for (int i = 0; i < el->props->len; i++) {
+        buffer[p++] = ' ';
+        Pair *pairs = (Pair *)el->props->items;
+        Pair *pair = pairs + i;
+        p += string_copy(buffer + p, pair->key);
+
+        buffer[p++] = '=';
+        buffer[p++] = '"';
+        p += string_copy(buffer + p, pair->value);
+        buffer[p++] = '"';
+    }
+
+    buffer[p++] = '>';
+
+    return p;
+}
+
+uint render(string buffer) {
+    Pair pairs[] = { "style", "color: red;", "foo", "bar" };
+    Vector props = { sizeof(pairs) / sizeof(Pair), pairs };
+    Element header = { "h2", &props, "Hello World" };
+
+    return toXml(buffer, &header);
 }
